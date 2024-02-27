93822a037e98c0517a141ef024e4a78625d985b2
abhishekjais333 <abhishekjais333@gmail.com>
2024-02-27 17:40:18 +0530

    first

--- avalon/src/App.jsx
+++ avalon/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
-
+import Nav from './Nav.jsx'
+import Selectlang from './components/Selectlang.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
     <h1 className='bg-red-900'></h1>
+    <Nav/>
+    <Selectlang/>
     </>
   )
 }
---
+++ avalon/src/components/Selectlang.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Selectlang() {
+  return (
+    <div className="flex justify-items-start items-center px-4 py-4">
+      <div className="w-[300px] rounded-md border px-2 py-2 border border-black">
+        <img
+          src="https://via.placeholder.com/300x200"
+          className="h-[200px] w-full rounded-md object-cover"
+          alt="Placeholder"
+        />
+        <div className="p-4">
+          <h1 className="text-lg font-semibold">English</h1>
+          <button
+            type="button"
+            className="mt-4 rounded-sm bg-black px-2.5 py-1 text-[10px] font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+          >
+            Start Learning
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Selectlang;
